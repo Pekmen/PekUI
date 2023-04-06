03cd807b948c813f19bfc0cd366f86e91b9847df
Pekmen <milanpekovic89@gmail.com>
2023-04-06 23:16:47 +0200

    updated package json with author data

--- packages/pekui-core/vite.config.ts
+++ packages/pekui-core/vite.config.ts
@@ -3,7 +3,6 @@ import path from "node:path";
 import react from "@vitejs/plugin-react";
 import dts from "vite-plugin-dts";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   build: {
     lib: {
