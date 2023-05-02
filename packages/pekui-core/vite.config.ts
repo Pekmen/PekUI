b29a0ce4db1e831bb2c3ec63e5ff8f5bfb4ac5e4
Pekmen <milanpekovic89@gmail.com>
2023-05-02 14:03:40 +0200

    prettier updates

--- packages/pekui-core/vite.config.ts
+++ packages/pekui-core/vite.config.ts
@@ -1,22 +1,22 @@
-import { defineConfig } from "vite";
-import path from "node:path";
-import react from "@vitejs/plugin-react";
-import dts from "vite-plugin-dts";
+import { defineConfig } from 'vite';
+import path from 'node:path';
+import react from '@vitejs/plugin-react';
+import dts from 'vite-plugin-dts';
 
 export default defineConfig({
   build: {
     lib: {
-      entry: path.resolve(__dirname, "src/lib/index.ts"),
-      name: "PekUI",
-      formats: ["es", "umd"],
+      entry: path.resolve(__dirname, 'src/lib/index.ts'),
+      name: 'PekUI',
+      formats: ['es', 'umd'],
       fileName: (format) => `pekui-core.${format}.js`,
     },
     rollupOptions: {
-      external: ["react", "react-dom"],
+      external: ['react', 'react-dom'],
       output: {
         globals: {
-          react: "React",
-          "react-dom": "ReactDOM",
+          react: 'React',
+          'react-dom': 'ReactDOM',
         },
       },
     },
